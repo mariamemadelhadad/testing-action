6e7a4a6cdd0930762f9a1e44edfebc06975db1df
mariam <mariam@aa.com>
2021-12-15 10:55:20 +0200

    Added source code files

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-class Hello
-{
-    public static void main(String args[])
- {
-     System.out.println("Hello World");
- }   
+class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
